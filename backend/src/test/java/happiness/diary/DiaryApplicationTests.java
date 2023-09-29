9d9551f29c0607b9be44bf5e68b8331d70d7571f
Sanghyeop Lee <d0003@naver.com>
2023-09-29 16:54:34 +0900

    SpringBoot 프로젝트 생성

---
+++ backend/src/test/java/happiness/diary/DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package happiness.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
