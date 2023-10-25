df9a93813b2a2df42b296f83d045e2caebac65cd
jmlee9707 <jmlee9707@naver.com>
2023-10-25 23:56:11 +0900

    fix: &gt; 추가

--- frontend/src/pages/result/index.tsx
+++ frontend/src/pages/result/index.tsx
@@ -12,11 +12,10 @@ const Result = () => {
 	const navigate = useNavigate();
 	const location = useLocation();
 	const [visible, setVisible] = useState(false);
-	const [mainSelected, setMainSelected] = useState<string>("");
-	const [subSelected, setSubSelected] = useState<string>("");
+	const [mainSelected, setMainSelected] = useState<string>('');
+	const [subSelected, setSubSelected] = useState<string>('');
 
 	useEffect(() => {
-
 		setQues(location.state);
 	}, []);
 
@@ -24,10 +23,10 @@ const Result = () => {
 		setMainSelected(str);
 	};
 	const handleClickSub = (str: string) => {
-		setSubSelected(str)
+		setSubSelected(str);
 	};
 	const handleSendFeedback = () => {
-		if(mainSelected && subSelected){
+		if (mainSelected && subSelected) {
 			navigate('/thanks');
 		}
 	};
@@ -37,7 +36,9 @@ const Result = () => {
 			<LogoSVGIcon />
 			<TextSection>
 				<TopText>
-					입력한 문장은 <span>"</span><span>{ques}</span><span>"</span> 이며
+					입력한 문장은 <span>"</span>
+					<span>{ques}</span>
+					<span>"</span> 이며
 				</TopText>
 				<QuesPart>
 					문장에서 <span>분노</span> 느껴집니다.
@@ -48,23 +49,33 @@ const Result = () => {
 				{visible && (
 					<BtnSection>
 						<p>가장 비슷한 감정을 선택해주세요 :)</p>
-						<p>> 대분류 중 하나를 선택해주세요</p>
+						<p>&gt; 대분류 중 하나를 선택해주세요</p>
 						<Buttons>
 							{FEELINGS_MAIN.map((feel) => (
-								<Chip key={feel} onClick={() => handleClickMain(feel)} $isTop = {true} $isSelected = {mainSelected===feel}>
+								<Chip
+									key={feel}
+									onClick={() => handleClickMain(feel)}
+									$isTop={true}
+									$isSelected={mainSelected === feel}
+								>
 									{feel}
 								</Chip>
 							))}
 						</Buttons>
-						<p>> 중분류 중 하나를 선택해주세요</p>
-						<Buttons >
+						<p>&gt; 중분류 중 하나를 선택해주세요</p>
+						<Buttons>
 							{FEELINGS_DETAIL.map((feel) => (
-								<Chip key={feel} $isTop = {false} onClick={() => handleClickSub(feel)} $isSelected = {subSelected===feel}>
+								<Chip
+									key={feel}
+									$isTop={false}
+									onClick={() => handleClickSub(feel)}
+									$isSelected={subSelected === feel}
+								>
 									{feel}
 								</Chip>
 							))}
 						</Buttons>
-						<SubmitBtn onClick = {handleSendFeedback}>결과 제출하기</SubmitBtn>
+						<SubmitBtn onClick={handleSendFeedback}>결과 제출하기</SubmitBtn>
 					</BtnSection>
 				)}
 			</TextSection>
@@ -84,7 +95,6 @@ const ResultContainer = styled.div`
 	flex-direction: column;
 `;
 const LogoSVGIcon = styled(LogoIcon)`
-
 	height: 100px;
 	margin-bottom: 60px;
 `;
@@ -107,16 +117,15 @@ const TopText = styled.div`
 	padding-bottom: 10px;
 
 	span {
-		display : inline-block;
+		display: inline-block;
 		font-weight: 700;
 		font-size: 28px;
 		color: var(--color-black);
-		max-width : 300px;
+		max-width: 300px;
 		line-height: 30px;
 		white-space: nowrap;
-		text-overflow: ellipsis;overflow:hidden;
-		
-
+		text-overflow: ellipsis;
+		overflow: hidden;
 	}
 `;
 const QuesPart = styled.div`
@@ -166,7 +175,7 @@ const BtnSection = styled.div`
 		&:first-child {
 			padding: 30px 0 10px 0;
 			font-size: 24px;
-			color : #515252
+			color: #515252;
 		}
 
 		line-height: 140%;
@@ -186,10 +195,10 @@ const Buttons = styled.div`
 	display: flex;
 	flex-wrap: wrap;
 	justify-content: center;
-	margin : 20px 0;
+	margin: 20px 0;
 `;
 
-const Chip = styled.button<{$isTop : boolean, $isSelected : boolean }>`
+const Chip = styled.button<{ $isTop: boolean; $isSelected: boolean }>`
 	line-height: 140%;
 	font-size: 18px;
 	font-family: var(--font-PRE);
@@ -199,25 +208,24 @@ const Chip = styled.button<{$isTop : boolean, $isSelected : boolean }>`
 	border-radius: 14px;
 	color: var(--color-white);
 	cursor: pointer;
-	background-color: ${(props)=>props.$isTop? "var(--color-pink)" :"var(--color-sub-pink)"} ;
-	box-shadow: ${(props)=>props.$isSelected? "0px 0px 15px 0px rgba(255, 60, 93, 1)": ""};
+	background-color: ${(props) => (props.$isTop ? 'var(--color-pink)' : 'var(--color-sub-pink)')};
+	box-shadow: ${(props) => (props.$isSelected ? '0px 0px 15px 0px rgba(255, 60, 93, 1)' : '')};
 
 	&:hover {
 		opacity: 0.8;
 	}
 `;
 
-const SubmitBtn  = styled.button`
-	color : var(--color-pink);
+const SubmitBtn = styled.button`
+	color: var(--color-pink);
 	font-size: 18px;
 	font-family: var(--font-PRE);
 	font-weight: 600;
 	line-height: 140%;
 	padding: 12px 20px;
-	border : 1px solid var(--color-pink);
+	border: 1px solid var(--color-pink);
 	border-radius: 20px;
-	margin-top : 50px;
+	margin-top: 50px;
 	background-color: var(--color-white);
-	cursor : pointer;
-	
-`
+	cursor: pointer;
+`;
